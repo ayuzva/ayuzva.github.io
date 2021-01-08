6a4984a51eaad541fae273e120f9a99fdca6b0cc
ayuzva <ayuzvaa@gmail.com>
2021-01-08 16:45:29 -0500

    Added project page

--- scripts/ayuzva.js
+++ scripts/ayuzva.js
@@ -1,3 +1,17 @@
 if($(window).width() <= 1199){
     $("#sidebar").hide();
 }
+
+$( function() {
+    var icons = {
+      header: "ui-icon-triangle-1-e",
+      activeHeader: "ui-icon-triangle-1-s"
+    };
+    $( "#project-list" ).accordion({
+      icons: icons
+    });
+    $( "#project-list" ).accordion( "option", "icons", icons );
+  } );
+
+
+
